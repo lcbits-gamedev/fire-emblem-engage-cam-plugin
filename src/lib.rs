ed7749c04e96c5429ce35ba9f3b544061a0b3113
lcbits-gamedev <gamedev@lcbits.com>
2024-09-08 15:11:01 +0200

    install hooks

--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,17 @@
 use engage::gamedata::unit::Unit;
 use engage::gamedata::PersonDataFields;
 
-#[skyline::main(name = "prevent_fade_plugin")]
+#[skyline::main(name = "cameraplugin")]
 pub fn main() {
-    println!("Prevent Unit Fading plugin initialized!");
-}
-
-#[unity::hook("App", "MapSequenceMind", "UnitDeadFade")]
-pub fn map_sequence_mind_unit_dead_fade(
-    this: &mut skyline::libc::c_void,
-    method: &skyline::libc::c_void,
-) {
-    println!("UnitDeadFade function called. Attempting to prevent fading.");
-
-    // Since we can't directly access the unit, we'll just log the interception
-    // and rely on other hooks to prevent fading
-}
-
-fn prevent_unit_fading(unit: &mut Unit) {
-    println!("Preventing fading for unit");
-
-    // We'll use other methods to ensure visibility since set_visible isn't available
-}
-
-#[unity::hook("App", "MapAction", "DeadBind")]
-pub fn map_action_dead_bind(this: &mut skyline::libc::c_void, unit: &mut Unit, param: i32) {
-    println!("DeadBind function called. Intercepting to prevent fading.");
-
-    prevent_unit_fading(unit);
+    println!("Prevent Unit Disappearance plugin initialized!");
+    
+    skyline::install_hooks!(
+        unit_die,
+        unit_set_visible,
+        person_set_die,
+        person_update,
+        unit_is_visible
+    );
 }
 
 #[unity::hook("App", "Unit", "Die")]
@@ -42,10 +26,7 @@ pub fn unit_die(this: &mut Unit) {
 
 #[unity::hook("App", "Unit", "SetVisible")]
 pub fn unit_set_visible(this: &mut Unit, visible: bool) {
-    println!(
-        "SetVisible called with value: {}. Forcing visibility.",
-        visible
-    );
+    println!("SetVisible called with value: {}. Forcing visibility.", visible);
     // Always set the unit to visible
     call_original!(this, true)
 }
